878b1202afe4f67869bef3241def40ba77fcc469
Mehrdad Amini <mehrdadamini1400@gmail.com>
2022-10-22 06:01:46 +0330

    add plt

--- logs/multywelllog.py
+++ logs/multywelllog.py
@@ -1,8 +1,9 @@
-def displalymulty(wells,logname):
+import matplotlib.pyplot as plt
+def multywelllog(wells,logname):
     fig, ax = plt.subplots(figsize=(8, 6))
     for label, data in wells:
-      data.logname.plot(kind = 'kde', ax = ax, label = label)
-      plt.xlim(data.logname.min(),data.logname.max())
+      data[logname].plot(kind = 'kde', ax = ax, label = label)
+      plt.xlim(data[logname].min(),data[logname].max())
     plt.grid(True)
     plt.legend()
     return plt.show()
